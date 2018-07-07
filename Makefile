ec327258d55a2ba4d0adb56ac28dd1a2d8056aa8
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2018-07-07 08:30:50 +0530

    auth

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 build:
 	GOOS=linux GOARCH=amd64 go build -o shipcon-user-cli
 	docker build -t shipcon-user-cli .
+	go clean
 
 run:
 	docker run --net="host" \
--- cli.go
+++ cli.go
@@ -1,73 +1,56 @@
 package main
 
 import (
-	"github.com/micro/go-micro/cmd"
+	"github.com/micro/go-micro"
 	pb "github.com/chauhanr/shipcon-user-service/proto/user"
 	microclient "github.com/micro/go-micro/client"
-	"github.com/micro/go-micro"
-	"github.com/micro/cli"
-	"golang.org/x/net/context"
+	"context"
 	"log"
 	"os"
 )
 
 func main(){
-	cmd.Init()
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.user-cli"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
 	client := pb.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
 
-	service := micro.NewService(
-		micro.Flags(
-			cli.StringFlag{
-				Name: "name",
-				Usage: "Your full name",
-			},
-			cli.StringFlag{
-				Name:  "email",
-				Usage: "Your email",
-			},
-			cli.StringFlag{
-				Name:  "password",
-				Usage: "Your password",
-			},
-			cli.StringFlag{
-				Name: "company",
-				Usage: "Your company",
-			},
-		),
-	)
+	name := "Ritesh Chauhan"
+	email := "ritesh.chauhan@hcl.com"
+	password := "test123"
+	company := "hcl"
 
-	service.Init(
-		micro.Action(func (c *cli.Context){
-			name := c.String("name")
-			email := c.String("email")
-			password := c.String("password")
-			company := c.String("company")
+	r, err := client.Create(context.TODO(), &pb.User{
+		Name: name,
+		Email: email,
+		Password: password,
+		Company: company,
+	})
 
-			r, err := client.Create(context.TODO(), &pb.User{
-				Name: name,
-				Email: email,
-				Password: password,
-				Company: company,
-			})
-			if err != nil {
-				log.Fatalf("Could not create: %v", err)
-			}
-			log.Printf("Created: %t", r.User.Id)
-
-			getAll, err := client.GetAll(context.Background(), &pb.User{})
-			if err != nil {
-				log.Fatalf("Could not list users: %v", err)
-			}
-			for _, v := range getAll.Users {
-				log.Println(v)
-			}
-
-			// let's just exit because
-			os.Exit(0)
-		}),
-	)
-
-	if err := service.Run(); err != nil{
-		log.Println(err)
+	if err != nil{
+		log.Fatalf("Could not create: %v", err)
 	}
+	log.Printf("Created: %s", r.User.Id)
+	getAll, err := client.GetAll(context.Background(), &pb.Request{})
+    if err != nil{
+		log.Fatalf("Could not list users: %v", err)
+	}
+
+	for _, v := range getAll.Users {
+		log.Println(v)
+	}
+
+	authResponse, err := client.Auth(context.TODO(), &pb.User{
+		Email: email,
+		Password: password,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not authenticate user: %s error: %v\n", email, err)
+	}
+	log.Printf("Your access token is : %s\n", authResponse.Token)
+	os.Exit(0)
 }
